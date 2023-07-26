18b6189cd28e0273560d3678661e74f005ddf418
v-wang <victorwang920@gmail.com>
2023-07-26 17:03:11 -0400

    function layout

---
+++ resources/script.js
@@ -0,0 +1,72 @@
+/*
+functions:
+  - start game
+  - start round
+  - add players
+  - round score calculator
+  - game score calculator
+  - die handler (how many dies are active to calculate)
+  - round over handler
+  
+  
+  idea2:
+  - triplet function (calculates score when there is a triplet - num * 100)
+  - 
+*/
+
+const COMBO_KEY = {
+  1: 100,
+  5: 50,
+  tri_2: 200,
+  tri_3: 300,
+  tri_4: 400,
+  tri_5: 500,
+  tri_6: 600,
+  four_kind: 1000,
+  five_kind: 2000,
+  six_kind: 3000,
+  straight: 1500,
+  tri_pair: 1500,
+  four_pair: 1500,
+  two_tri: 2500,
+};
+
+let ROUND_SCORE = 0;
+let PLAYER_TOTAL_SCORE = 0;
+
+// score of 250
+let myRoll = [1, 1, 2, 5, 4];
+// score of 200
+let myNextRoll = [1, 1];
+// total score of 450
+let myTotalRoundScore = 0;
+
+function roll() {
+  // randomly generate die rolls
+}
+
+function translateDieToCombos(arr) {
+  // arr = [2,2,2,1,1,3]
+  // output: ["tri_2",1,1]
+}
+
+function calculateComboScore(arr) {
+  // take in current round die values - in arr form
+  // arr = [3,1,6,2,1,3]
+}
+
+function addToRoundScoreTotal(num) {
+  // add to ROUND_SCORE
+}
+
+function resetRoundScoreTotal() {
+  ROUND_SCORE = 0;
+}
+
+function farkle(arr) {
+  // determines if current roll (arr) is a farkle
+}
+
+function disableDie(die) {
+  // disables die from rolling
+}
